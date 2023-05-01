adba7d6f95a2f084192a33dd15b30548d320f0fa
hafssarawi <78560300+hafssarawi@users.noreply.github.com>
2023-05-01 19:48:25 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import discord
+import AllCountries
+import funfacts
+import guessCapital
+import requests
+import os
+import discord
+import random
+import asyncio
+from discord.ext import commands
+
+guessCapital_channel_id = 1101662911031685152
+funfacts_channel_id = 1102037939900395571
+countries_channel_id = 1102025012740898896
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+bot.add_command(guessCapital)
+
+my_secret = os.environ['DISCORD_BOT_SECRET']
+bot.run(my_secret)
